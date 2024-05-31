238e3be294c09f35728f0fe80d520c8d6b442af7
malvertis1ng <ydarkpvp150934@gmail.com>
2024-05-31 17:52:07 -0300

    features

--- script/scriptCardGenerator.js
+++ script/scriptCardGenerator.js
@@ -1,4 +1,4 @@
-const typeColor = {
+const tipoCor = {
     bug: "#26de81",
     dragon: "#ffeaa7",
     electric: "#fed330",
@@ -20,69 +20,69 @@ const typeColor = {
   const card = document.getElementById("card");
   const btn = document.getElementById("btn");
   
-  let getPokeData = () => {
+  let getAPI = () => {
     let id = Math.floor(Math.random() * 150) + 1;
     const finalUrl = url + id;
     fetch(finalUrl)
       .then((response) => response.json())
       .then((data) => {
-        geradorCard(data);
+        gerarCard(data);
       });
   };
+
   
-  
-  let geradorCard = (data) => {
+  let gerarCard = (data) => {
     console.log(data);
     const hp = data.stats[0].base_stat;
     const imgSrc = data.sprites.other.dream_world.front_default;
-    const pokeName = data.name[0].toUpperCase() + data.name.slice(1);
-    const statAttack = data.stats[1].base_stat;
-    const statDefense = data.stats[2].base_stat;
-    const statSpeed = data.stats[5].base_stat;
+    const pokeNome = data.name[0].toUpperCase() + data.name.slice(1);
+    const statAtaque = data.stats[1].base_stat;
+    const statDefesa = data.stats[2].base_stat;
+    const statVelocidade = data.stats[5].base_stat;
   
-    const themeColor = typeColor[data.types[0].type.name];
-    console.log(themeColor);
+    const fundoCor = tipoCor[data.types[0].type.name];
+    console.log(fundoCor);
     card.innerHTML = `
           <p class="hp">
             <span>HP</span>
               ${hp}
           </p>
           <img src=${imgSrc} />
-          <h2 class="poke-name">${pokeName}</h2>
+          <h2 class="poke-name">${pokeNome}</h2>
           <div class="types">
            
           </div>
           <div class="stats">
             <div>
-              <h3>${statAttack}</h3>
+              <h3>${statAtaque}</h3>
               <p>Ataque</p>
             </div>
             <div>
-              <h3>${statDefense}</h3>
+              <h3>${statDefesa}</h3>
               <p>Defesa</p>
             </div>
             <div>
-              <h3>${statSpeed}</h3>
+              <h3>${statVelocidade}</h3>
               <p>Velocidade</p>
             </div>
           </div>
     `;
-    appendTypes(data.types);
-    styleCard(themeColor);
+    tipos(data.types);
+    estiloCard(fundoCor);
   };
-  let appendTypes = (types) => {
+  let tipos = (types) => {
     types.forEach((item) => {
       let span = document.createElement("SPAN");
       span.textContent = item.type.name;
       document.querySelector(".types").appendChild(span);
     });
   };
-  let styleCard = (color) => {
+  let estiloCard = (color) => {
     card.style.background = `radial-gradient(circle at 50% 0%, ${color} 36%, #ffffff 36%)`;
-    card.querySelectorAll(".types span").forEach((typeColor) => {
-      typeColor.style.backgroundColor = color;
+    card.querySelectorAll(".types span").forEach((tipoCor) => {
+      tipoCor.style.backgroundColor = color;
     });
   };
   
-  btn.addEventListener("click", getPokeData);
-  window.addEventListener("load", getPokeData);
+  btn.addEventListener("click", getAPI);
+  window.addEventListener("load", getAPI);
